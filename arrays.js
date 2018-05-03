8b3555b2b94943b0aee0e5894c92b1aa27f6bf6b
Lucas <lucsbasto@gmail.com>
2018-05-03 15:11:27 -0300

    funções concat, push, pop, unshift, shift, slice e splice

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var arr = [1, 2, 3, 4];
+console.log(arr.concat(5)); //cria e retorna um array temporario com o parametro adicionado sem modificar o array principal
+arr.push(6);//adiciona o parametro no array principal
+
+arr.unshift('x');//adiciona o parametro no começo do array
+arr.shift();//remove e retorna o primeiro item;
+
+//array.slice(inicio, final)
+console.log(arr.slice(0, 3));//retorna do 1 ao 3 item do array sem modificar o array
+

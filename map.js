ad3019486cd8203acb12d120f799a03954e1157b
Lucas <lucsbasto@gmail.com>
2018-05-03 16:17:04 -0300

    metodo map

---
+++ map.js
@@ -0,0 +1,7 @@
+var arr = [1, 2, 3, 4, 5, 6, 7, 8 ,9 , 10];
+
+var map = arr.map(function(item, index, array){ //percorre o array e faz uma operação
+    return {index: index, item: item};
+});
+
+console.log(map);

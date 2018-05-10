abfae92ef7d037ec3a738fc76ab6030e57b03a1b
Lucas <lucsbasto@gmail.com>
2018-05-10 13:43:03 -0300

    alguns metodos de string

--- map.js
+++ map.js
@@ -3,5 +3,4 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8 ,9 , 10];
 var map = arr.map(function(item, index, array){ //percorre o array e faz uma operação e retorna um novo array
     return {index: index, item: item};
 });
-
 console.log(map);
---
+++ string.js
@@ -0,0 +1,11 @@
+var nome = 'arara';
+var arr = nome.split('a');
+console.log(nome.charAt(4));
+console.log(nome.indexOf('s'));
+
+console.log(arr);
+var nome1 = arr.join('a'); //junta o array substituindo os separadores pela letra a
+
+console.log(nome1)
+console.log(nome.toLowerCase());
+console.log(nome.toUpperCase());

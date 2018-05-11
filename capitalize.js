795c38af297743d5b5b5215319c77a0c3d4c8b91
Lucas <lucsbasto@gmail.com>
2018-05-10 21:01:40 -0300

    add simplificação de codigo no swap

--- capitalize.js
+++ capitalize.js
@@ -9,10 +9,10 @@ function capitalize(string){
 
 
 function capitalize_(s){
-    //  /b[a-z]/ pega a primeira letra de a-z da palavra    o parametro 'a' já é o achado pela expressão regular
+    //  /b[a-z]/ pega a primeira letra de a-z da palavra    o parametro 'a' já é o achado pela expressão regular e passado como callback
+    //pela função replace
     return s.toLowerCase().replace(/\b[a-z]/g, function(a){ return a.toUpperCase(); });
 };
-
 
 console.log (capitalize('this IS THE wOrst string eVeR'));
 console.log (capitalize_('this IS THE wOrst string eVeR'));
--- swapCase.js
+++ swapCase.js
@@ -1,11 +1,8 @@
 function swapCase(string){
     return string.replace(/(\w)/g, function(express, a, c){
-        if(a === a.toLowerCase()){
-            return a.toUpperCase();
-        }
-        else{
-            return a.toLowerCase();
-        }
+        console.log("Arguments: ", arguments);
+        if(a === a.toLowerCase()){return a.toUpperCase();}
+        else{return a.toLowerCase();}
         // (c%2 == 0) ? nome += a.toUpperCase() : nome += a.toLowerCase(); //RETORNAR StRiNg
         // return nome;
     });

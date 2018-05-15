85463c292889516ac3ac89d53aec6abbb773a7d8
Lucas <lucsbasto@gmail.com>
2018-05-15 15:12:59 -0300

    challenge 20 resolvido

--- Browser/window.js
+++ Browser/window.js
@@ -1,10 +1,11 @@
 (function(win, doc){
     'use strict';
-    //querySelector e querySelectorAll são estáticos
-    var $input = doc.querySelectorAll('.input') ; //boa pratica iniciar variáveis do doc com $
-    var $button = doc.querySelector('#button'); //chama o atributo como no css
-
-    console.log(`Usando o querySelectorAll() ele pega todos os que tiver o atributo, no caso a classe input ${$input}`);
-    console.log(`Usando o querySelector() ele pega somente a primeira aparição do atributo, no caso o id ${button}`);
-
+    var $input = doc.querySelector('#input');
+    var $pass = doc.querySelector("#password");
+    var $button = doc.querySelector("#button");
+    //addEventListener(evento, função callback, false);
+    $button.addEventListener('submit', function(event){
+        event.preventDefault();
+        console.log($input.value, $pass.value);
+    }, false);
 })(window, document);
